91fdc83a75604b3c8ad53035ffa222b29f4ce3bb
khalidghanamy <khalid.gamal.hamed@gmail.com>
2022-05-07 22:59:33 +0200

    client router update

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-
+import React from 'react';
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Register from './pages/Register';
+import Chat from './pages/Chat';
+import Login from './pages/Login';
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/" element={<Chat/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
