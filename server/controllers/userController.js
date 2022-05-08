c385bbadb2c784504073460c3d42a26595f86b71
khalidghanamy <khalid.gamal.hamed@gmail.com>
2022-05-08 13:28:23 +0200

    login make it cleaner

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,60 +1,49 @@
-const Users =require("../model/userModel")
-const brcypt= require("bcrypt")
-
-const sendError=(data)=>{
-    if(!data)
-    return res.json({msg:"Incorrect username or password",status:false})
-}
-module.exports={
-    
-    register:async (req,res,next)=>{
-const {username,email,password}=req.body
-try {
-    
-const usernameCheck = await Users.findOne({username})
-
-if(usernameCheck)
-return res.json({msg:"user already exist",status:false})
-
-
-const emailCheck =await Users.findOne({email})
-console.log(email,emailCheck);
-if(emailCheck)
-return res.json({msg:"email already exist",status:false})
-
-const hashedPassword=await brcypt.hash(password,10);
-const user =await Users.create({
-    username,
-    email,
-    password:hashedPassword
-})
-delete user.password;
-return res.json({status:true,user})
-
-} catch (error) {
-    next(error)
-}
-},
-
-login:async (req,res,next)=>{
-    const {username,password}=req.body
+const Users = require("../model/userModel");
+const brcypt = require("bcrypt");
+//send error if not valid
+const sendError = (data = 1, res = "") => {
+  if (!data)
+    return res.json({ msg: "Incorrect username or password", status: false });
+};
+module.exports = {
+  register: async (req, res, next) => {
+    const { username, email, password } = req.body;
     try {
-        
-    const user = await Users.findOne({username})
-    
-    if(!user)
-    return res.json({msg:"Incorrect username or password",status:false})
-    const isPasswordValid= await brcypt.compare(password,user.password)
+      const usernameCheck = await Users.findOne({ username });
 
-    sendError(isPasswordValid);
-    delete user.password;  
-    return res.json({status:true,user})
-    
+      sendError(!usernameCheck, res);
+
+      const emailCheck = await Users.findOne({ email });
+      console.log(email, emailCheck);
+      sendError(!emailCheck, res);
+
+      const hashedPassword = await brcypt.hash(password, 10);
+      const user = await Users.create({
+        username,
+        email,
+        password: hashedPassword,
+      });
+      delete user.password;
+      return res.json({ status: true, user });
     } catch (error) {
-        next(error)
+      next(error);
     }
+  },
+
+  login: async (req, res, next) => {
+    const { username, password } = req.body;
+    try {
+      const user = await Users.findOne({ username });
+
+      sendError(user, sendError);
+      const isPasswordValid = await brcypt.compare(password, user.password);
+
+      sendError(isPasswordValid, res);
+
+      delete user.password;
+      return res.json({ status: true, user });
+    } catch (error) {
+      next(error);
     }
-
-
-
-}
+  },
+};
