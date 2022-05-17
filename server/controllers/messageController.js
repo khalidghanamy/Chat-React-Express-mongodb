e88f7f7b1f904567f7a15685cfde8ef734a48d9a
khalidghanamy <khalid.gamal.hamed@gmail.com>
2022-05-17 02:22:38 +0200

    server - messagecontroller updated

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,2 +1,20 @@
-module.exports.addMessage=async (req,res,next)=>{}
+
+const Messages =require("../model/messageModel")
+module.exports.addMessage=async (req,res,next)=>{
+
+    try {
+const {from,to,message}= req.body;
+const data =await Messages.create({
+    message:{text:message},
+    users:{from,to},
+    sender:from
+
+});
+if(data) return res.json({msg:"message add successfully"})
+return res.json({msg: "failed to add messages"})
+    } catch (error) {
+
+        next(error)
+    }
+}
 module.exports.getAllmessages=async (req,res,next)=>{}
