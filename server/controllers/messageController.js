cdd0aefb55405b62c4103da27e2a3a31461e5d27
khalidghanamy <khalid.gamal.hamed@gmail.com>
2022-05-17 02:51:24 +0200

    server - message controller updated

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -17,4 +17,23 @@ return res.json({msg: "failed to add messages"})
         next(error)
     }
 }
-module.exports.getAllmessages=async (req,res,next)=>{}
+module.exports.getAllmessages=async (req,res,next)=>{
+
+    try {
+        const {from,to}= req.body;
+         const messages = await  Messages.find({
+            users:{
+                $all:[from,to]
+            },
+         })
+         .sort({updatedAt:1})
+         const projectMessages = messages.map((msg)=>{
+                return{
+                    fromSelf:msg.sender.toString() === from,
+                    message: msg.message.text
+                }
+         })
+    } catch (error) {
+        
+    }
+}
