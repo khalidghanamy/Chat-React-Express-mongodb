8cd67e220c08d53deaef33ad58993b1115b71d26
khalidghanamy <khalid.gamal.hamed@gmail.com>
2022-05-17 03:08:07 +0200

    updated

--- client/src/components/ChatHere.jsx
+++ client/src/components/ChatHere.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React ,{useState,useEffect}from "react";
 import styled from "styled-components";
 import Logout from "./Logout";
 import Chatinput from "./Chatinput";
 import axios from "axios"
 import Messages from "./Messages";
 import { sendMessageRoute } from "../utils/ApiRoutes";
+import { getAlldMessageRoute } from "../utils/ApiRoutes";
 function ChatHere({ currentChat ,currentUser}) {
+  const [messages, setMessages] = useState([])
+  useEffect(()=>{
+    async function getAllMessages (){
+      const response = await axios.post(getAlldMessageRoute,{
+        from: currentUser._id,
+        to:currentChat._id
+      })
+      setMessages(response.data)
+    }
+    getAllMessages()
+  },[currentChat])
     const handleSendMsg=async (msg)=>{
       await axios.post(sendMessageRoute,{
         from:currentUser._id,
@@ -32,7 +44,21 @@ function ChatHere({ currentChat ,currentUser}) {
           <Logout />
           </div>
         
-              <Messages/>
+              <div className="chat-messages">
+                {
+                  messages.map((message)=>{
+                      return(
+                        <div>
+                          <div className={`message ${message.fromSelf? "sended":"recieved"} `} >
+                            <p>
+                              {message.message}
+                            </p>
+                          </div>
+                          </div>
+                      )
+                  })
+                }
+              </div>
       
           <Chatinput handleSendMsg={handleSendMsg}/>
           <div className="chat-input"></div>
